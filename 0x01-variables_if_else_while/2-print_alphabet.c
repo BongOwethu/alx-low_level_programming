bf79d6da32e83c314fe9f0f11674fc0c35ce676f
Mandisa Shange <mandisampume@lotlook.com>
2023-02-17 15:20:02 +0200

    A program that prints the alphabet in lowercase, followed by a new line.

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ * Description: Get a random value with 5
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	char c = 'a';
+
+	while (c <= 'z'
+	{
+		putchar(c);
+		c++;
+	}
+	putchar('\n');
+	return (0);
+}
