2cd5fcc4756ba4891a31b0c3c4155c975b69a666
Mandisa Shange <mandisampume@lotlook.com>
2023-02-17 14:58:23 +0200

    This program will assign a random number to the variable n each time it is executed. Complete the source code in order to print whether the number stored in the variable n is positive or negative.

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -4,20 +4,19 @@
 /**
  * main - Entry point
  * Description: Get a random number and print the number
- * and if it is positive, negavtive or zer
+ * and finnd if it is positive, negative or zer
  * Return: 0 (Success)
  */
 int main(void)
 {
 	int n;
 
-	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	if (n > 0)
 		printf("%i is positive\n", n);
 	else if (n < 0)
 		printf("%i is negative\n", n);
 	else
-		print"%i is zer\n", n);
+		printf("%i is zero\n", n);
 	return (0);
 }
--- 0x01-variables_if_else_while/README.md
+++ 0x01-variables_if_else_while/README.md
@@ -1,1 +1,1 @@
- This is a file for the 0x01 C  - Variables, if, else, while tasks which are from 0-9
+This is a README file for the 0x01. C - Variables, if, else, while from task 0-9
