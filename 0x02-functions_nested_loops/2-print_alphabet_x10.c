57dd99b4a726007d51977992ef784fd010776d9e
Mandisa Shange <mandisampume@lotlook.com>
2023-02-21 10:08:40 +0200

    A C program that prints the alphabet 10 times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * main -Entr point
+ * Decription: Print the alphabet 10 times
+ * Return: void
+ */
+void print_alphabet_x10(void)
+{
+	char c;
+	int i = 0;
+
+	while (i < 10)
+	{
+		c = 'a';
+
+		while (c <='z')
+		{
+			_putchar(c);
+			c++;
+		}
+		_putchar('\n');
+		i++;
+	}
+}
