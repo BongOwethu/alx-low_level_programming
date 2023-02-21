6bd8afc363e708d88d7a38af7344c64a0943092f
Mandisa Shange <mandisampume@lotlook.com>
2023-02-21 10:14:11 +0200

    A C program that prints the alphabet 10 times

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -13,7 +13,7 @@ void print_alphabet_x10(void)
 	{
 		c = 'a';
 
-		while (c <='z')
+		while (c <= 'z')
 		{
 			_putchar(c);
 			c++;
---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ * Description: Check for the lowercase letter
+ * character to check the case
+ * Return: 0 or 1
+ */
+
+int _islower(int c)
+{
+	return (c >= 97 && c <= 127);
+}
