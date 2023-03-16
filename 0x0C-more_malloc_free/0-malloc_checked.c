a1ce7e9b30613ab952dbed747678114fc86171a3
Mandisa Shange <mandisampume@lotlook.com>
2023-03-16 12:37:25 +0200

    Update

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -8,12 +8,13 @@
  */
 void *malloc_checked(unsigned int b)
 {
-	void *ptr;
+	void *p;
 
-	ptr = malloc(b);
-
-	if (ptr == NULL)
+	p = malloc(b);
+	if (p == NULL)
+	{
 		exit(98);
+	}
 
 	return (ptr);
 }
