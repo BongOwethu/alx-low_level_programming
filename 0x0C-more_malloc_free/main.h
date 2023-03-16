abbbfd0a79e956863ea92c9138fc2ef268913490
Mandisa Shange <mandisampume@lotlook.com>
2023-03-16 11:58:37 +0200

    Update

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,15 +1,19 @@
 #ifndef MAIN_H
-	#define MAIN_H
+#define MAIN_H
 
-	#include <stdio.h>
-	#include <stdlib.h>
-	#include <limits.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
 
-	int _putchar(char c);
-	void *malloc_checked(unsigned int b);
-	char *string_nconcat(char *s1, char *s2, unsigned int n);
-	void *_calloc(unsigned int nmemb, unsigned int size);
-	int *array_range(int min, int max);
-	void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
+int _putchar(char c);
+void *malloc_checked(unsigned int b);
+char *string_nconcat(char *s1, char *s2, unsigned int n);
+void *_calloc(unsigned int nmemb, unsigned int size);
+int *array_range(int min, int max);
+int is_valid_number(char *num)
+int multiply(char *num1, char *num2)
+int main(int argc, char **argv)
 
-	#endif
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
+
+#endif
