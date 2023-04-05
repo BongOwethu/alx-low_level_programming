3849d10a0aa1cdf94858d28022c090926ecff617
Mandisa Shange <mandisampume@lotlook.com>
2023-04-05 11:47:39 +0200

    Updated code in files

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,3 +1,4 @@
+#include "lists.h"
 
 /**
  * list_len - gets the number of nodes
@@ -9,13 +10,21 @@
 
 size_t list_len(const list_t *h)
 {
-	size_t n = 0;
+        size_t node_count = 1;
 
-	while (h)
-	{
-		n++;
-		h = h->next;
-	}
-	return (n);
+        /*if head is null return 0 nodes*/
+        if (h == NULL)
+                return (0);
+
+        /*iterate through the nodes*/
+        while (h->next != NULL)
+        {
+                /*go to the next node*/
+                h = h->next;
+                /*count node*/
+                node_count++;
+        }
+
+        return (node_count);
 }
 
