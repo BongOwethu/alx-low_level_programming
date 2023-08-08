1ec5d88a470520607963aa2de02da81edce6cb8a
Mandisa Nompumelelo Shange <123155517+BongOwethu@users.noreply.github.com>
2023-08-08 10:35:31 +0200

    Update _putchar.c

--- 0x0C-more_malloc_free/_putchar.c
+++ 0x0C-more_malloc_free/_putchar.c
@@ -2,8 +2,8 @@
 /**
  * _putchar - writes the character c to stdout
  * @c: The character to print
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
+ * Return: 1 succes.
+ * -1 for fail is returned.
  */
 int _putchar(char c)
 {
